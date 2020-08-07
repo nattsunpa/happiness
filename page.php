1999641e364ad389f7b4b5c735754a8a3853752f
Natsumi Kosaki <nattsunpa@gmail.com>
2020-08-07 11:39:09 +0900

    料金ページの作成

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<main class="price">
+<h1 class="price__top-title">料金案内<br>Price</h1>
+<div class="price__inner">
+    <p class="price__inner--txt">
+    HappinessでWeb制作のお仕事を受注させていただいた場合の料金のご案内です。<br>
+    こちらは目安となりますので、詳しい依頼内容をご相談いただければ、詳細なお見積もりを出させていただきます。
+    </p>
+    <dl class="price__inner--list">
+        <dt class="price__item--left">バナー作成</dt>
+        <dd class="price__item--right">￥99,999～</dd>
+        <dt class="price__item--left">Webサイトデザイン</dt>
+        <dd class="price__item--right">￥99,999～</dd>
+        <dt class="price__item--left">Webサイト作成（HTML/CSS/JS）</dt>
+        <dd class="price__item--right">￥999,999～</dd>
+        <dt class="price__item--left">Webサイト作成（WordPress）</dt>
+        <dd class="price__item--right">￥999,999～</dd>
+    </dl>
+    <div class="price__inner--button">
+        <a href="price__button">お問い合わせフォーム</a>
+    </div>
+</div>
+
+
+</main>
+<?php get_footer(); ?>
